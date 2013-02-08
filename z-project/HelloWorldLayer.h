64b4c31d5ec49b4616f79d0deed2bc983f6612af
Aurélien Noce <aurnoce@gmail.com>
2013-02-09 00:41:20 +0100

    Initial Commit

--- z-project/HelloWorldLayer.h
+++ z-project/HelloWorldLayer.h
@@ -7,12 +7,10 @@
 //
 
 
-#import <GameKit/GameKit.h>
 
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
 #import "Box2D.h"
-#import "GLES-Render.h"
 
 //Pixel to metres ratio. Box2D uses metres as the unit for measurement.
 //This ratio defines how many pixels correspond to 1 Box2D "metre"
@@ -21,11 +19,8 @@
 #define PTM_RATIO 32
 
 // HelloWorldLayer
-@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+@interface HelloWorldLayer : CCLayer
 {
-	CCTexture2D *spriteTexture_;	// weak ref
-	b2World* world;					// strong ref
-	GLESDebugDraw *m_debugDraw;		// strong ref
 }
 
 // returns a CCScene that contains the HelloWorldLayer as the only child
