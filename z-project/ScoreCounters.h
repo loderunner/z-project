e2155ca2a8c80c3e4e66b60559e713aeb37cbd4d
Aurélien Noce <aurnoce@gmail.com>
2013-02-09 20:56:17 +0100

    Created class to hold scores

---
+++ z-project/ScoreCounters.h
@@ -0,0 +1,13 @@
+//
+//  ScoreCounters.h
+//  z-project
+//
+//  Created by Aurélien Noce on 09/02/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScoreCounters : NSObject
+
+@end
---
+++ z-project/ScoreCounters.m
@@ -0,0 +1,13 @@
+//
+//  ScoreCounters.m
+//  z-project
+//
+//  Created by Aurélien Noce on 09/02/13.
+//
+//
+
+#import "ScoreCounters.h"
+
+@implementation ScoreCounters
+
+@end
