4fd3443e832477acceab67784d0171940a2b73c6
Aurélien Noce <aurnoce@gmail.com>
2013-02-09 20:23:15 +0100

    starting main menu

---
+++ z-project/MainMenuLayer.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuLayer.h
+//  z-project
+//
+//  Created by Aurélien Noce on 09/02/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuLayer : CCNoce
+
+@end
---
+++ z-project/MainMenuLayer.m
@@ -0,0 +1,13 @@
+//
+//  MainMenuLayer.m
+//  z-project
+//
+//  Created by Aurélien Noce on 09/02/13.
+//
+//
+
+#import "MainMenuLayer.h"
+
+@implementation MainMenuLayer
+
+@end
