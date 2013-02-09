14215f5673607a2439502b7087461bad8d052336
Aurélien Noce <aurnoce@gmail.com>
2013-02-09 20:57:19 +0100

    First working version of the score counters

--- z-project/ScoreCounters.h
+++ z-project/ScoreCounters.h
@@ -10,4 +10,23 @@
 
 @interface ScoreCounters : NSObject
 
+// initial properties
+@property (nonatomic,readonly) NSInteger numZombiesInitial;
+@property (nonatomic,readonly) NSInteger numCiviliansInitial;
+
+// generated properties
+@property (nonatomic,readonly) NSInteger numZombies;
+@property (nonatomic,readonly) NSInteger numCivilians;
+@property (nonatomic,readonly) NSInteger numCiviliansKilledByPlayer;
+@property (nonatomic,readonly) NSInteger numCiviliansConvertedToZombie;
+@property (nonatomic,readonly) NSInteger numZombiesSpawned;
+@property (nonatomic,readonly) NSInteger numZombiesKilledByPlayer;
+
+-(void)registerCivilianConvertedToZombie;
+-(void)registerZombieKilledByPlayer;
+-(void)registerCivilianKilledByPlayer;
+-(void)registerZombieSpawned;
+
+-(id)initWithZombies:(NSInteger)zombies civilians:(NSInteger)civilians;
+
 @end
