8bc35cb34efc264fed2c2e9c0c67ec06233c0041
Raphael P <raphael@imagine-app.fr>
2013-02-10 11:28:32 +0100

    Add class sound manager

---
+++ z-project/SoundManager.h
@@ -0,0 +1,13 @@
+//
+//  SoundManager.h
+//  z-project
+//
+//  Created by Raphael on 10/02/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoundManager : NSObject
+
+@end
---
+++ z-project/SoundManager.m
@@ -0,0 +1,13 @@
+//
+//  SoundManager.m
+//  z-project
+//
+//  Created by Raphael on 10/02/13.
+//
+//
+
+#import "SoundManager.h"
+
+@implementation SoundManager
+
+@end
