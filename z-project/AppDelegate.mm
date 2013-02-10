9266b0e1830cb1f833ce6537facbbd20c3d0abf8
Aurélien Noce <aurnoce@gmail.com>
2013-02-10 18:35:17 +0100

    remove fps and set framerate to 30

--- z-project/AppDelegate.mm
+++ z-project/AppDelegate.mm
@@ -39,10 +39,10 @@
 	director_.wantsFullScreenLayout = YES;
 	
 	// Display FSP and SPF
-	[director_ setDisplayStats:YES];
+	//[director_ setDisplayStats:YES];
 	
 	// set FPS at 60
-	[director_ setAnimationInterval:1.0/60];
+	[director_ setAnimationInterval:1.0/30];
 	
 	// attach the openglView to the director
 	[director_ setView:glView];
